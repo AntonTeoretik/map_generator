7a96bbdef396977a02551e67d4225381bb4185a6
Anton <a.d.ablov@gmail.com>
2023-06-16 16:34:09 +0200

    Code clean

--- src/main/kotlin/com/teoretik/geometry/rays/processors/PointRayProcessor.kt
+++ src/main/kotlin/com/teoretik/geometry/rays/processors/PointRayProcessor.kt
@@ -3,16 +3,13 @@ package com.teoretik.geometry.rays.processors
 import com.badlogic.gdx.math.Intersector
 import com.badlogic.gdx.math.Polygon
 import com.badlogic.gdx.math.Vector2
-import com.teoretik.components.obstacles.Obstacle
 import com.teoretik.geometry.rays.HitResult
 
 class PointRayProcessor() : RayProcessor {
     override fun processRay(start: Vector2, end: Vector2, obstacles: Sequence<Polygon>): HitResult {
         obstacles.forEach {
             val intersects = Intersector.intersectSegmentPolygon(start, end, it)
-//            if (intersects && !it.contains(end)) return HitResult.MISS
             if (intersects) return HitResult.MISS
-
         }
         return HitResult.HIT
     }
--- src/main/kotlin/com/teoretik/geometry/rays/processors/RayProcessor.kt
+++ src/main/kotlin/com/teoretik/geometry/rays/processors/RayProcessor.kt
@@ -2,8 +2,6 @@ package com.teoretik.geometry.rays.processors
 
 import com.badlogic.gdx.math.Polygon
 import com.badlogic.gdx.math.Vector2
-import com.teoretik.geometry.shapes.Shape
-import com.teoretik.components.obstacles.Obstacle
 import com.teoretik.geometry.rays.HitResult
 
 interface RayProcessor {
