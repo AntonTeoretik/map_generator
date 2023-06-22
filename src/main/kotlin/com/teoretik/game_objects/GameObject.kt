093822673d3841526d03be53be99f1a9de798ad4
Anton <a.d.ablov@gmail.com>
2023-06-22 21:30:00 +0200

    Draft for game objects

--- src/main/kotlin/com/teoretik/game_objects/GameObject.kt
+++ src/main/kotlin/com/teoretik/game_objects/GameObject.kt
@@ -1,4 +1,26 @@
 package com.teoretik.game_objects
 
-class GameObject {
+interface Context
+interface Reaction
+
+interface Movable {
+    fun setNewCoordinates(newCoordinates : Pair<Int, Int>)
+}
+
+interface Big {
+
+}
+
+sealed class Action {
+    object NotAnAction
+    //...
+}
+
+interface Intractable {
+    fun processAction(action : Action, context : Context) : Reaction
+}
+
+
+abstract class GameObject {
+    val mapCoordinates : Pair<Int, Int> = 0 to 0
 }
